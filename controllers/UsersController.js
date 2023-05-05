b51bdce65c974cd59e4332f3bb3862a63f4b3c37
kushal-kamra <kushalkamradev@gmail.com>
2023-05-05 17:03:16 +0530

    Linter

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -45,6 +45,7 @@ const UsersController = {
         data: userDetails,
       });
     } catch (err) {
+      console.error(err);
       return res.status(500).send({
         success: false,
         message: 'login failed.',
